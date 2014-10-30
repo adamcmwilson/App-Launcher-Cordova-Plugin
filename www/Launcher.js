c8bb9e089e1a526c83b28fd4c06def9fc9dcd222
Mathieu Bouchard <matb33@gmail.com>
2014-10-30 10:31:41 -0400

    fix some issues

--- www/Launcher.js
+++ www/Launcher.js
@@ -1,13 +1,19 @@
+console.log("[Launcher] installing...");
+
 var exec = require("cordova/exec");
 
 function Launcher() {}
 
 Launcher.prototype.canLaunch = function (options, successCallback, errorCallback) {
+	console.log("[Launcher] canLaunch: " + JSON.stringify(options));
     cordova.exec(successCallback, errorCallback, "Launcher", "canLaunch", [options]);
 };
 
 Launcher.prototype.launch = function(options, successCallback, errorCallback) {
+	console.log("[Launcher] launch: " + JSON.stringify(options));
     cordova.exec(successCallback, errorCallback, "Launcher", "launch", [options]);
 };
 
 module.exports = new Launcher();
+
+console.log("[Launcher] installed.");
